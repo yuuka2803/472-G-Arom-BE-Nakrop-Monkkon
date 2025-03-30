yuuka2803/472-S-Arom-BE-Nakrop-Monkkon-6510405768

Init Go Server, Add:Fiber, Air, Postgresql, Pgadmin

configs/config.go
@@ -1,0 +1,32 @@
+package configs
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB_HOST    		string `mapstructure:"DB_HOST"`
+	DB_NAME  		string `mapstructure:"DB_NAME"`
+	DB_USER  		string `mapstructure:"DB_USER"`
+	DB_PASSWORD 	string `mapstructure:"DB_PASSWORD"`
+	DB_PORT 		string `mapstructure:"DB_PORT"`
+	ADMIN_EMAIL 	string `mapstructure:"ADMIN_EMAIL"`
+	ADMIN_PASSWORD 	string `mapstructure:"ADMIN_PASSWORD"`
+}
+
+func NewConfig() *Config {
+	config := &Config{}
+	viper.SetConfigFile(".env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln("❌ Error reading config file", err)
+	}
+
+	if err := viper.Unmarshal(config); err != nil {
+		log.Fatalln("❌ Unable to decode into struct", err)
+	}
+
+	return config
+}

yuuka2803/472-G-Arom-BE-Nakrop-Monkkon

configs/config.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Config struct {
-	DB_HOST    		string `mapstructure:"DB_HOST"`
-	DB_NAME  		string `mapstructure:"DB_NAME"`
-	DB_USER  		string `mapstructure:"DB_USER"`
-	DB_PASSWORD 	string `mapstructure:"DB_PASSWORD"`
-	DB_PORT 		string `mapstructure:"DB_PORT"`
-	JWT_SECRET	string `mapstructure:"JWT_SECRET"`
+	DB_HOST          string `mapstructure:"DB_HOST"`
+	DB_NAME          string `mapstructure:"DB_NAME"`
+	DB_USER          string `mapstructure:"DB_USER"`
+	DB_PASSWORD      string `mapstructure:"DB_PASSWORD"`
+	DB_PORT          string `mapstructure:"DB_PORT"`
+	JWT_SECRET       string `mapstructure:"JWT_SECRET"`
 	SUPABASE_URL     string `mapstructure:"SUPABASE_URL"`
 	SUPABASE_API_KEY string `mapstructure:"SUPABASE_API_KEY"`
 	SUPABASE_BUCKET  string `mapstructure:"SUPABASE_BUCKET"`